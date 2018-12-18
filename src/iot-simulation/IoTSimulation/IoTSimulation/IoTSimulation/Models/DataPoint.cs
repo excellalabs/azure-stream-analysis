a0c2d0fde3f4bc7f3005d2a31a1fe81f9b834616
Alexander Slotte <alexander.slotte@gmail.com>
2018-12-17 20:45:25 -0500

    Added initial IoT simulation

---
+++ src/iot-simulation/IoTSimulation/IoTSimulation/IoTSimulation/Models/DataPoint.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IoTSimulation.Models
+{
+    internal sealed class DataPoint
+    {
+        public string DeviceId { get; set; }
+
+        public SensorReadings SensorReadings { get; set; }
+    }
+}
---
+++ src/iot-simulation/IoTSimulation/IoTSimulation/IoTSimulation/Program.cs
@@ -0,0 +1,36 @@
+﻿using Excella.TwitterClient.Business.EventHub;
+using IoTSimulation.Models;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+
+namespace IoTSimulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting IoT simulation");
+            var eventHubService = new EventHubService();
+
+            while (true)
+            {
+                var dataPoint = new DataPoint
+                {
+                    DeviceId = "SimulatedDevice",
+                    SensorReadings = new SensorReadings
+                    {
+                        Temperature = 20.5m,
+                        Humidity = 80,
+                        LightLevel = 300,
+                        SoundLevel = new Random().Next(0, 100),
+                        Distance = new Random().Next(1, 200)
+                    }
+                };
+
+                eventHubService.SendMessageAsync(dataPoint);
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
