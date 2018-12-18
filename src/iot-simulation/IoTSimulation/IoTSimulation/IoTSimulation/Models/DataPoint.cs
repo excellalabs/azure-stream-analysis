c2f6c414a301d221e6d315328f9166352e3782e7
Alexander Slotte <alexander.slotte@gmail.com>
2018-12-18 17:47:01 -0500

    Added config for IoTSimulation

--- src/iot-simulation/IoTSimulation/IoTSimulation/IoTSimulation/Models/DataPoint.cs
+++ src/iot-simulation/IoTSimulation/IoTSimulation/IoTSimulation/Models/DataPoint.cs
@@ -1,13 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace IoTSimulation.Models
+﻿namespace IoTSimulation.Models
 {
     internal sealed class DataPoint
     {
         public string DeviceId { get; set; }
 
         public SensorReadings SensorReadings { get; set; }
+
+        public override string ToString()
+        {
+            return $"DeviceId: {DeviceId}, " +
+                $"Temperature: {SensorReadings.Temperature}, " +
+                $"Humidity: {SensorReadings.Humidity}, " +
+                $"SoundLevel: {SensorReadings.SoundLevel}, " +
+                $"LightLevel: {SensorReadings.LightLevel}," +
+                $"Distance: {SensorReadings.Distance}";
+        }
     }
 }
