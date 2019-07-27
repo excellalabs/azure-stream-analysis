54e1fb001f5db6dd567ef3cfdd809129eec4c1da
Alexander Slotte <alexander.slotte@gmail.com>
2019-07-27 09:55:11 -0400

    Removed obsolete webjob and azure functions (replaced by logic apps)

--- src/webjob/Excella.TwitterWebJob/Configuration/JobHostConfigurationSettings.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Excella.TwitterWebJob.Configuration
-{
-    public class JobHostConfigurationSettings
-    {
-        public string StorageAccountConnectionString { get; set; }
-        public string ApplicationInsightInstrumentKey { get; set; }
-    }
-}
